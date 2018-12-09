b3aefd3ba35e3a803961aea21c3939ed4c898fff
zep1994 <snaxbags@protonmail.com>
2018-12-08 21:18:23 -0600

    ReverseString

---
+++ index.js
@@ -0,0 +1,18 @@
+function reverseString(str) {
+  return strArr = str.split('').reverse().join('')
+}
+
+function isPalindrome(str) {
+
+}
+
+function reverseInt(int) {
+
+}
+
+
+
+
+const output = reverseString('hello')
+
+console.log(output)
